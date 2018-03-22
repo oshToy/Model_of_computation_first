a315ec7d05bad377b50715746b2f9b4c04398f1e
USER <USER@DESKTOP-RD7IS2S>
2018-03-22 13:34:35 +0200

    uploading the second file

---
+++ 308247865/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+import ac.il.afeka.Submission.Submission;
+
+public class Main implements Submission, Assignment1 {
+
+	@Override
+	public List<String> submittingStudentIds() {
+		return Arrays.asList("308247865", "204004055");
+	}
+
+	@Override
+	public boolean compute(String dfsmEncoding, String input) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
